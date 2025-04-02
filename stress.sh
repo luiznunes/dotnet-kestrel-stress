982771b204821eac59a157633667adb8f5f45e1f
Luiz Gustavo Nunes <luiz.nunes@grupovoalle.com.br>
2025-04-02 10:59:55 -0300

    .

--- stress.sh
+++ stress.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker exec -it Dotnet-Kestrel curl http://localhost/api/stress
-docker exec -it Dotnet-Kestrel curl http://localhost/api/stress
-docker exec -it Dotnet-Kestrel curl http://localhost/api/stress
+docker exec -it Dotnet-Kestrel curl http://localhost/api/stress & docker exec -it Dotnet-Kestrel curl http://localhost/api/stress & docker exec -it Dotnet-Kestrel curl http://localhost/api/stress
