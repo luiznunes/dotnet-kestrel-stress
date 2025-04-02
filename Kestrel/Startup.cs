5ca19768affd195833ab5644e601308af65e2b08
Luiz Gustavo Nunes <luiz.nunes@grupovoalle.com.br>
2025-04-02 10:27:34 -0300

    Update

--- Kestrel/Startup.cs
+++ Kestrel/Startup.cs
@@ -50,7 +50,7 @@ namespace Kestrel
                 app.UseHsts();
             }
 
-            // MemoryStress();
+            MemoryStress();
 
             app.UseHttpsRedirection();
             app.UseMvc();
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build . -t dotnet-kestrel
+docker build --no-cache . -t dotnet-kestrel
 
 echo "Execução: docker run --rm --memory=1gb --name=Dotnet-Kestrel --cpuset-cpus=0 dotnet-kestrel"
