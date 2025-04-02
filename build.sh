0cad8112cf65b32cf088b88b6a2e3ce634fc6264
Luiz Gustavo Nunes <luiz.nunes@grupovoalle.com.br>
2025-04-02 11:20:46 -0300

    .

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build --no-cache . -t dotnet-kestrel
 
-echo "Execução: docker run --rm --memory=1gb --name=Dotnet-Kestrel --cpuset-cpus=0 dotnet-kestrel"
+echo "Execução: docker run --rm --memory=5gb --memory-swappiness=0 --name=Dotnet-Kestrel --cpuset-cpus=0 dotnet-kestrel"
